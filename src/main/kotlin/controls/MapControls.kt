633fd5101178823a603edce846737e7e1c73c87a
kamilchulakov <mr1english4@gmail.com>
2023-09-07 00:14:21 +0300

    #3 Move input query to map controls

--- src/main/kotlin/controls/MapControls.kt
+++ src/main/kotlin/controls/MapControls.kt
@@ -1,10 +1,15 @@
 package controls
+import org.openqa.selenium.Keys
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.FindBy
 import page.Page
 
 class MapControls(private val driver: WebDriver): Page(driver) {
+    // строка поиска
+    @FindBy(xpath = "//input")
+    private val searchInput: WebElement? = null
+
     // кнопка пробок
     @FindBy(css = ".traffic-raw-icon__text")
     private val trafficButton: WebElement? = null
@@ -12,6 +17,11 @@ class MapControls(private val driver: WebDriver): Page(driver) {
     // кнопка с тремя точками
     @FindBy(xpath = "//div[2]/div/button")
     private val extButton: WebElement? = null
+
+    fun inputQuery(login: String?) {
+        // TODO: #%
+        searchInput!!.sendKeys(login, Keys.ENTER)
+    }
 
     fun clickTrafficButton() {
         // TODO: #5
--- src/main/kotlin/page/Page.kt
+++ src/main/kotlin/page/Page.kt
@@ -1,21 +1,9 @@
 package page
 
-import org.openqa.selenium.Keys
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.WebElement
-import org.openqa.selenium.support.FindBy
 import org.openqa.selenium.support.PageFactory
 
 open class Page(driver: WebDriver) {
-
-    // строка поиска
-    @FindBy(xpath = "//input")
-    private val searchInput: WebElement? = null
-
-    fun inputQuery(login: String?) {
-        searchInput!!.sendKeys(login, Keys.ENTER)
-    }
-
     init {
         PageFactory.initElements(driver, this)
     }
