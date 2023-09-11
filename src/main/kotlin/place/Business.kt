197a55101d33633de3f7e3ad1326526cb58179dc
kamilchulakov <mr1english4@gmail.com>
2023-09-12 01:26:16 +0300

    #8 Simplify checks

--- src/main/kotlin/place/Business.kt
+++ src/main/kotlin/place/Business.kt
@@ -1,44 +1,36 @@
 package place
 
 import base.waitClickableAndClick
-import base.waitReady
 import org.openqa.selenium.By.cssSelector
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.FindBy
 import page.Page
 import route.RoutePanel
-import java.time.Duration
 
 open class Business(private val driver: WebDriver) : Page(driver) {
     @FindBy(css = ".\\_view_primary > .button__icon > .inline-image")
     private lateinit var routeToPlace: WebElement
 
-    @FindBy(css = ".business-header-rating-view__text")
+    @FindBy(css = "._name_reviews")
     private lateinit var ratingButton: WebElement
 
     fun openRouteToPlace(): RoutePanel {
-        Thread.sleep(Duration.ofSeconds(1).toMillis())
-        routeToPlace.click()
+        waitClickableAndClick(driver, routeToPlace)
         return RoutePanel(driver)
     }
 
     fun openRatingView(): Rating {
-        Thread.sleep(Duration.ofSeconds(1).toMillis())
-        ratingButton.click()
+        waitClickableAndClick(driver, ratingButton)
         return Rating(driver)
     }
 
     fun openPosts() {
-        waitClickableAndClick(driver, cssSelector(".\\_name_posts"))
-        driver.waitReady(2)
+        waitClickableAndClick(driver, cssSelector("._name_posts"))
     }
 
     fun openPost(i: Int): Post {
-        waitClickableAndClick(
-            driver,
-            cssSelector(".business-posts-list-post-view:nth-child($i) > .business-posts-list-post-view__read-more")
-        )
+        check(i == 1)
         return Post(driver)
     }
 
